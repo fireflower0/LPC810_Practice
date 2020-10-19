c8748c78d8daef8943797bfaf2093b64676542a3
fireflower0 <fireflower0@hotmail.co.jp>
2020-10-19 20:46:53 +0900

    Fixed a blink

--- blink/src/main.c
+++ blink/src/main.c
@@ -1,13 +1,14 @@
 #include "LPC8xx.h"
 
+/* スイッチ・マトリクスの設定 */
 void SwitchMatrix_Init() {
-  // Enable SWM clock
+  /* Enable SWM clock */
   LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 7);
 
-  // Pin Assign 8 bit Configuration
+  /* Pin Assign 8 bit Configuration */
   // none
 
-  // Pin Assign 1 bit Configuration
+  /* Pin Assign 1 bit Configuration */
   // RESET
   LPC_SWM->PINENABLE0 = 0xffffffbfUL;
 }
@@ -15,18 +16,25 @@ void SwitchMatrix_Init() {
 int main() {
   volatile int8_t SW1;
 
+  /* スイッチ・マトリクス設定 */
   SwitchMatrix_Init();
 
-  LPC_GPIO_PORT->DIR0 |= (1 << 2);
-  LPC_GPIO_PORT->DIR0 &= ~(1 << 1);
+  /* ポートの方向決定 */
+  LPC_GPIO_PORT->DIR0 |= (1 << 2);  // PIO0_2を出力ポートに設定
+  LPC_GPIO_PORT->DIR0 &= ~(1 << 1); // PIO0_1を入力ポートに設定
 
   while(1) {
+    /* ポートの入力レベル(High/Low)の読み込み            */
+    /* スイッチ入力SW1 (ON : 0 (Low), OFF : 1 (High)) */
     SW1 = (LPC_GPIO_PORT->PIN0 & (1 << 1)) >> 1;
 
+    /* ポートの出力レベルの決定 */
     if (SW1 == 1) {
-      LPC_GPIO_PORT->CLR0 = 1 << 2;
+      /* スイッチ入力SW1 (OFF) */
+      LPC_GPIO_PORT->CLR0 = 1 << 2; // PIO0_2をLowに設定 (LED消灯)
     } else {
-      LPC_GPIO_PORT->SET0 = 1 << 2;
+      /* スイッチ入力SW1 (ON) */
+      LPC_GPIO_PORT->SET0 = 1 << 2; // PIO0_2をHighに設定 (LED点灯)
     }
   }
 }
